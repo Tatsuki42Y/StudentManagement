06f2d4d6fd04c6b8907a883e9f30879430ce3b8b
Tatsuki42Y <m.book.0042@gmail.com>
2025-10-03 19:16:13 +0900

    パッケー ジ名を修正

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,19 +1,27 @@
-package raisetech.StudentManagement;
+package raisetech.studentmanagement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.Student;
+import raisetech.studentmanagement.StudentRepository;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentManagementApplication.class, args);
-	}
-@GetMapping("/whatsup")
-  public String hello(){
-    return "Whats Up!, World!";
-}
+  @Autowired
+  private StudentRepository repository;
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudentManagementApplication.class, args);
+  }
+
+  @GetMapping("/studentList")
+  public List<Student> getStudentList() {
+    return repository.search();
+  }
 }
