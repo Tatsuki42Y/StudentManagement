fd2d4718979f9ae816dbb705b788be50ab54c679
Tatsuki42Y <m.book.0042@gmail.com>
2025-11-29 23:14:17 +0900

    課題33のPR再作成
    PR再作成しました。
    Command + Option + L（⌘ + ⌥ + L）で全体をフォーマットしています。
    ご確認よろしくお願いします。

---
+++ main/java/raisetech/studentmanagement/domain/StudentDetail.java
@@ -0,0 +1,18 @@
+package raisetech.studentmanagement.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import raisetech.studentmanagement.data.Student;
+import raisetech.studentmanagement.data.StudentsCourses;
+
+@Getter
+@Setter
+public class StudentDetail {
+
+  private Student student;
+  private List<StudentsCourses> studentsCourses;
+}
+
+
