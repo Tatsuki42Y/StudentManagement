0eab5d999054846f3f50d4772025bcf2244ee0c1
Tatsuki42Y <m.book.0042@gmail.com>
2026-02-21 22:22:27 +0900

    リセット

---
+++ StudentManagement/src/main/java/raisetech/studentmanagement/controller/handler/TestExceptionHandler.java
@@ -0,0 +1,17 @@
+package raisetech.studentmanagement.controller.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import raisetech.studentmanagement.exception.TestException;
+
+@ControllerAdvice
+public class TestExceptionHandler {
+
+  @ExceptionHandler(TestException.class)
+  public ResponseEntity<String> handleTestException(TestException ex) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+}
+
---
+++ StudentManagement/src/main/java/raisetech/studentmanagement/service/StudentService.java
@@ -0,0 +1,113 @@
+package raisetech.studentmanagement.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import raisetech.studentmanagement.controller.converter.StudentConverter;
+import raisetech.studentmanagement.data.CourseStatus;
+import raisetech.studentmanagement.data.Student;
+import raisetech.studentmanagement.data.StudentCourse;
+import raisetech.studentmanagement.domain.StudentDetail;
+import raisetech.studentmanagement.exception.TestException;
+import raisetech.studentmanagement.repository.StudentRepository;
+
+/**
+ * 受講生情報を取り扱うサービスです。 受講生の検索や登録・更新処理を行います。
+ */
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+  private StudentConverter converter;
+
+  @Autowired
+  public StudentService(StudentRepository repository, StudentConverter converter) {
+    this.repository = repository;
+    this.converter = converter;
+  }
+
+  /**
+   * 受講生詳細の一覧検索です。 全件検索を行うので、条件指定は行いません。
+   *
+   * @return 受講生詳細一覧（全件）
+   */
+  public List<StudentDetail> searchStudentList() {
+    List<Student> studentList = repository.search();
+    List<StudentCourse> studentCourseList = repository.searchStudentCourseList();
+    List<CourseStatus> courseStatusList = repository.searchCourseStatusList();
+    return converter.convertStudentDetails(studentList, studentCourseList, courseStatusList);
+  }
+
+
+  /**
+   * 受講生詳細の検索です。 IDに紐づく受講生情報を取得したあと、その受講生に紐づく受講生コース情報を取得して設定します。
+   *
+   * @param id 受講生ID
+   * @return 受講生詳細
+   */
+  public StudentDetail searchStudent(String id) {
+    Student student = repository.searchStudent(id);
+    if (student == null) {
+      throw new TestException("IDが存在しません");
+    }
+    List<StudentCourse> studentCourse = repository.searchStudentCourse(student.getId());
+    List<CourseStatus> courseStatusList = repository.searchCourseStatusByStudentId(student.getId());
+    return new StudentDetail(student, studentCourse,courseStatusList);
+  }
+
+  /**
+   * 受講生詳細の登録を行います。
+   * 受講生と受講生登録情報を個別に登録し、受講生コース情報には受講生情報を紐づける値やコース開始日、コース終了日を設定します。
+   * @param studentDetail　受講生詳細
+   * @return 登録情報を付与した受講生詳細
+   */
+  @Transactional
+  public StudentDetail registerStudent(StudentDetail studentDetail) {
+    Student student = studentDetail.getStudent();
+    repository.registerStudent(student);
+    studentDetail.getStudentCourseList().forEach(studentCourse -> {
+      initStudentsCourse(studentCourse, student);
+      repository.registerStudentCourse(studentCourse);
+    });
+    return studentDetail;
+  }
+
+  /**
+   *受講生コース情報を登録する際の初期情報を登録する。
+   * @param studentCourse　受講生コース情報
+   * @param student　受講生
+   */
+  private void initStudentsCourse(StudentCourse studentCourse, Student student) {
+    LocalDateTime now = LocalDateTime.now();
+    studentCourse.setStudentId(student.getId());
+    studentCourse.setCourseStartAt(now);
+    studentCourse.setCourseEndAt(now.plusYears(1));
+  }
+
+  /**
+   * 受講生詳細の更新を行います。受講生と受講生コース情報をそれぞれ更新します。
+   *
+   * @param studentDetail　受講生詳細
+   */
+  @Transactional
+  public void updateStudent(StudentDetail studentDetail) {
+    repository.updateStudent(studentDetail.getStudent());
+    studentDetail.getStudentCourseList()
+        .forEach(studentCourse -> repository.updateStudentCourse(studentCourse));
+  }
+
+  /**
+   * 受講生コース状況の更新を行います。
+   *
+   * @param studentDetail　受講生詳細
+   */
+  @Transactional
+  public void updateCourseStatus(StudentDetail studentDetail) {
+    repository.updateStudent(studentDetail.getStudent());
+    studentDetail.getCourseStatusList()
+        .forEach(courseStatus -> repository.updateCourseStatus(courseStatus));
+  }
+}
